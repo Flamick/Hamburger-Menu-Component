c300f037e152c47200bbefe2a7f0d4055c015972
Flamick <adhd_ninja7@Yahoo.com>
2024-01-29 20:07:51 -0500

    Fixed button functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,28 @@
-const menuBtn = document.querySelector('.menu-btn');
-const mobileMenu = document.querySelector('#mobile-menu');
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector("#mobile-menu");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-if(!menuOpen) {
-    menuBtn.classList.add('open');
-    mobileMenu.classList.add('open');
-    menuOpen = true;
-} else {
-    menuBtn.classList.remove('open');
-    mobileMenu.classList.remove('open');
-    menuOpen = false;
-}
+let isTransitioning = false;
+menuBtn.addEventListener("click", () => {
+	const lastMenuItem = mobileMenu.lastElementChild.lastElementChild;
+	if (isTransitioning) {
+		return;
+	} else if (!menuOpen) {
+		isTransitioning = true;
+		mobileMenu.classList.remove("closed");
+		menuBtn.classList.add("open");
+		mobileMenu.classList.add("open");
+		menuOpen = true;
+		lastMenuItem.addEventListener("transitionend", () => {
+			isTransitioning = false;
+		});
+	} else {
+		isTransitioning = true;
+		menuBtn.classList.remove("open");
+		mobileMenu.classList.remove("open");
+		mobileMenu.classList.add("closed");
+		menuOpen = false;
+		lastMenuItem.addEventListener("transitionend", () => {
+			isTransitioning = false;
+		});
+	}
 });
